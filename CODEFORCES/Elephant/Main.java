98b79583fae406c4330db945b8d79a74c5cdcbd4
tomaszkoziolek <mes.tomaszkoziolek@gmail.com>
2024-02-28 23:55:38 +0100

    ewq

--- CODEFORCES/Elephant/Main.java
+++ CODEFORCES/Elephant/Main.java
@@ -6,6 +6,9 @@ public class Main {
       int a = scanner.nextInt();
       scanner.close();
 
+        //chcemy aby rezultat byl intem, a metoda Math.ceil() dziala na typach zmiennoprzecinkowych
+        //zatem int a 'rzutujemy' na double a (rzutowanie typow)
+        //jako ze Math.ceil() zwroci double rzutujemy cala ta operacje na 'int' aby wynik pasowal do typu zmiennej
       int result = (int) Math.ceil((double) a / 5);
       
       System.out.println(result);
